069bf7cbf7caa769dc4d2a5f5e807b24056f923e
LeonardoRuzza <10608001@polimi.it>
2022-04-24 18:09:46 +0200

    Fixing name of the SC

--- contracts/FederatedML.sol
+++ contracts/FederatedML.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.13;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ReceptionistFederatedML is Ownable {
+contract FederatedML is Ownable {
     enum STATE {
         FUNDING,
         REGISTERING,
