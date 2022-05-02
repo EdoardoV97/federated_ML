87cfc0ef0a409a27c9ba51cc3477d65a38ea9804
EdoardoV97 <10570524@polimi.it>
2022-05-02 16:58:08 +0200

    SC implementation

--- contracts/FederatedML.sol
+++ contracts/FederatedML.sol
@@ -2,13 +2,20 @@
 
 pragma solidity ^0.8.13;
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
+import "@chainlink/contracts/src/v0.6/ChainlinkClient.sol";
 
-contract FederatedML is Ownable {
+contract FederatedML is Ownable, VRFConsumerBase, ChainlinkClient {
+    //
+    using EnumerableSet for EnumerableSet.AddressSet;
+
     enum STATE {
         FUNDING,
         REGISTERING,
-        TASK_STARTED,
-        TASK_ENDED
+        TASK_ENDED,
+        TASK_ABORTED,
+        ROUND_PREPARATION
     }
     struct WorkerInfo {
         uint256 fee;
@@ -20,7 +27,6 @@ contract FederatedML is Ownable {
     struct Round {
         address[] workers;
     }
-    event RoundWorkersSelection(address[] workers);
 
     STATE public state;
     string taskScript;
@@ -30,18 +36,37 @@ contract FederatedML is Ownable {
     uint256 entranceFee;
     uint256 bounty;
     address coordinatorSC;
+    // To keep track of the rounds
     Round[] rounds;
+    // Variables for the vrf_coordinator
+    uint256 public fee;
+    bytes32 public keyhash;
+    // Variables for the api_oracle
+    address private oracleApiAddress;
+    bytes32 private jobId;
 
     mapping(address => uint256) public addressToAmountFunded;
-    address[] public funders;
+    EnumerableSet.AddressSet public funders;
     mapping(address => WorkerInfo) public addressToWorkerInfo;
-    address[] public workers;
+    EnumerableSet.AddressSet public workers;
+
+    EnumerableSet.AddressSet private residualWorkers;
+
+    // Events
+    event RequestedRandomness(bytes32 requestId); // For the VRF coordinator
+    event RoundWorkersSelection(address[] workers);
 
     constructor(
         string memory _taskScript,
         uint16 _workersNumber,
-        uint16 _roundsNumber
-    ) {
+        uint16 _roundsNumber,
+        address _vrfCoordinator,
+        address _link,
+        uint256 _fee,
+        bytes32 _keyhash,
+        address _oracleApiAddress,
+        bytes32 _jobId
+    ) public VRFConsumerBase(_vrfCoordinator, _link) {
         require(
             _workersNumber % _roundsNumber == 0,
             "The number of workers must be a multiple of the number of rounds!"
@@ -50,18 +75,25 @@ contract FederatedML is Ownable {
         workersNumber = _workersNumber;
         roundsNumber = _roundsNumber;
         state = STATE.FUNDING;
+        fee = _fee;
+        keyhash = _keyhash;
+        oracleApiAddress = _oracleApiAddress;
+        jobId = _jobId;
     }
 
     function fund() public payable {
         require(state == STATE.FUNDING, "Is not possible to further fund!");
         addressToAmountFunded[msg.sender] += msg.value;
-        funders.push(msg.sender);
+        add(funders, msg.sender);
     }
 
     function stopFunding() public onlyOwner {
         require(state == STATE.FUNDING, "Is not possible to stop funding now!");
         state = STATE.REGISTERING;
         entranceFee = computeFee();
+        // TODO verify there are enough LINK tokens
+
+        // TODO Start a timer, after which we need to cancel the task and refund all funders, workers and the admin
     }
 
     function computeFee() internal returns (uint256) {
@@ -76,10 +108,9 @@ contract FederatedML is Ownable {
             "You are already registered!"
         ); // It requires that entranceFee is not 0
         addressToWorkerInfo[msg.sender].fee += msg.value;
-        workers.push(msg.sender);
-        if (workers.length >= workersNumber) {
-            state = STATE.TASK_STARTED;
-            startRound();
+        add(workers, msg.sender);
+        if (length(workers) >= workersNumber) {
+            initializeRound();
         }
     }
 
@@ -98,7 +129,7 @@ contract FederatedML is Ownable {
 
     function unfund() public {
         require(
-            state == STATE.FUNDING,
+            state == STATE.FUNDING || state == STATE.TASK_ABORTED,
             "Not possible to unfund in this phase!"
         );
         require(
@@ -107,12 +138,12 @@ contract FederatedML is Ownable {
         );
         msg.sender.transfer(addressToAmountFunded[msg.sender]);
         addressToAmountFunded[msg.sender] = 0;
-        removeFunder(msg.sender);
+        remove(funders, msg.sender);
     }
 
     function unregister() public {
         require(
-            state == STATE.REGISTERING,
+            state == STATE.REGISTERING || state == STATE.TASK_ABORTED,
             "Not possible to unregister in this phase!"
         );
         require(
@@ -122,19 +153,7 @@ contract FederatedML is Ownable {
         msg.sender.transfer(addressToWorkerInfo[msg.sender].fee);
         addressToWorkerInfo[msg.sender].fee = 0;
         // remove from the list of
-        removeWorker(msg.sender);
-    }
-
-    function removeWorker(address addr) internal {
-        for (uint16 i = 0; i < workers.length; i++) {
-            if (workers[i] == addr) {
-                if (workers.length > 1) {
-                    workers[i] = workers[workers.length - 1];
-                }
-                workers.length--;
-                return;
-            }
-        }
+        remove(workers, msg.sender);
     }
 
     function removeFunder(address addr) internal {
@@ -149,15 +168,18 @@ contract FederatedML is Ownable {
         }
     }
 
-    function startRound() internal {
-        //TODO
+    function initializeRound() internal {
         // Request random numbers to the oracle to choose the workers for the round
+        state = STATE.ROUND_PREPARATION;
+        bytes32 requestId = requestRandomness(keyhash, fee);
+        emit RequestedRandomness(requestId);
     }
 
-    function nextRound() internal {
+    function startRound(uint256 _randomness) internal {
         //TODO
         // Given the random numbers use it as indexes to get the workers (take next if already choose)
         // Emit an event with the choosen workers for the round
+        // Start the timer for the round duration
     }
 
     function getPreviousModels() public view returns (string[]) {
@@ -188,5 +210,14 @@ contract FederatedML is Ownable {
         // Check if the workers is one of the last round and it has committed a secret vote
         // Check the validity of the vote
         // If it is the last disclosure, end the task
+    }
+
+    function fulfillRandomness(bytes32 _requestId, uint256 _randomness)
+        internal
+        override
+    {
+        require(state == STATE.ROUND_PREPARATION, "You aren't there yet!");
+        require(_randomness > 0, "random-not-found");
+        startRound(_randomness);
     }
 }
