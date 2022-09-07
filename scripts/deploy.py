8fce694e6781009f730ef1f92203f35ab01b25a8
LeonardoRuzza <10608001@polimi.it>
2022-09-07 16:54:57 +0200

    Added deploy ZK version

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,4 +1,4 @@
-from brownie import accounts, config, FederatedML, network
+from brownie import accounts, config, FederatedML, FederatedML_ZK, network
 from scripts.helpful_scripts import fund_with_link, get_account, get_contract
 
 
@@ -32,6 +32,32 @@ def deploy_FederatedML():
     print(f"Contract deployed to: {federatedML_contract.address}")
 
     return federatedML_contract
+
+
+def deploy_FederatedML_ZK():
+    vrf_coordinator = get_contract("vrf_coordinator").address
+    link_token = get_contract("link_token").address
+    oracle_fee = config["networks"][network.show_active()]["fee"]
+    keyhash = config["networks"][network.show_active()]["keyhash"]
+    api_oracle = config["networks"][network.show_active()]["api_oracle"]
+    job_id = config["networks"][network.show_active()]["job_id"]
+
+    account = get_account()
+
+    # Deploy
+    federatedML_ZK_contract = FederatedML_ZK.deploy(
+        INITIAL_MODEL_HASH,
+        vrf_coordinator,
+        link_token,
+        oracle_fee,
+        keyhash,
+        api_oracle,
+        job_id,
+        {"from": account},
+    )
+    print(f"Contract deployed to: {federatedML_ZK_contract.address}")
+
+    return federatedML_ZK_contract
 
 
 def main():
