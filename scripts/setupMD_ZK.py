9deb96b3d73448bc98ab8c26c38dc4b8bf4d7aca
LeonardoRuzza <10608001@polimi.it>
2022-09-08 14:08:26 +0200

    Splitted two files for branching

---
+++ scripts/setupMD_ZK.py
@@ -0,0 +1,33 @@
+# This script is needed to deploy the SC to testnets or ganacheUI
+
+from brownie import config, network
+from scripts.helpful_scripts import fund_with_link, get_account
+from scripts.deploy import deploy_FederatedML_ZK
+
+FUND_QUANTITY: 10
+
+
+def main():
+    federatedML_contract = deploy_FederatedML_ZK()
+    account = get_account()
+    oracle_fee = config["networks"][network.show_active()]["fee"]
+
+    link_amount = oracle_fee * 10  # 0.1 * 10 = 1 LINK
+
+    # Fund with link
+    tx = fund_with_link(federatedML_contract.address, amount=link_amount)
+    tx.wait(1)
+
+    # Fund the bounty
+    fund_quantity = 10  # 10 Wei
+    tx = federatedML_contract.fund({"from": account, "value": fund_quantity})
+    tx.wait(1)
+
+    print("Stopping funding!")
+    # Stop funding phase
+    tx = federatedML_contract.stopFunding({"from": account})
+    tx.wait(1)
+
+
+if __name__ == "__main__":
+    main()
